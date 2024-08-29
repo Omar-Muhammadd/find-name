9e350f697c935e3130665b64ec92ec8a48a5b5d4
omar-muhammadd <omaar5538@gmail.com>
2024-08-28 18:52:11 -0700

    fire

---
+++ main.js
@@ -0,0 +1,31 @@
+
+var names = prompt("Enter your name: ")
+
+document.body.innerHTML = `
+    <img src="assets/1.gif" id="fire">
+    <h1 id="h1Name">${names}</h1>
+
+    `
+
+var fire = document.getElementById("fire")
+
+document.addEventListener('mousemove', function(e){
+    fire.style.position = "absolute"
+    fire.style.left = e.clientX-50 + "px"
+    fire.style.top = e.clientY-5 + "px"
+})
+
+
+
+var nameRun = document.getElementById("h1Name")
+
+function run(){
+    var X = Math.random() * 700 
+    var Y = Math.random() * 700
+
+    nameRun.style.transform = `translate(${X}px,${Y}px)`
+ }
+
+nameRun.addEventListener('mouseenter',() => {
+    setTimeout(run, 500)
+} )
